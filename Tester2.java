f11a1357b759847fe6d42a273038a740c0bbabcf
SamuraiZach <zach.stuff01@gmail.com>
2021-03-28 20:27:28 -0700

    Adding prints

--- Tester2.java
+++ Tester2.java
@@ -6,6 +6,8 @@ public class Tester2 {
         Boat[] bArr = new Boat[2];
         bArr[0] = new Submarine(1,new Coordinates(4,4), 3, 4);
         bArr[1] = new AircraftCarrier(2,new Coordinates(5,5), 1);
+        System.out.println(bArr[0]);
+        System.out.println(bArr[1].locationx);
         String map = w1.drawTeamMap(bArr, 2);
         System.out.println(map);
     }
