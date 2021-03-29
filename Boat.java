8f29d177cb43b17d438f65e1a76bdb60ce99e251
SamuraiZach <zach.stuff01@gmail.com>
2021-03-28 20:51:46 -0700

    Adding proper turn and variables

--- Boat.java
+++ Boat.java
@@ -4,6 +4,7 @@ public abstract class Boat
     private int team, direction, health, strength, vision;
     public Coordinates obj;
     public int locationx, locationy;
+    public String directo;
     public Boat(int teamm, int directionn, int healthh, int strengthh, int visionn, Coordinates location)
     {
         team = teamm;
@@ -19,6 +20,24 @@ public abstract class Boat
         return team;
     }
     public int getDirection(){
+        if(direction == 0){
+            directo = "NORTH";
+        }else if(direction == 1){
+            directo = "NORTHEAST";
+        }else if(direction == 2){
+            directo = "EAST";
+        }else if(direction == 3){
+            directo = "SOUTHEAST";
+        }else if(direction == 4){
+            directo = "SOUTH";
+        }else if(direction == 5){
+            directo = "SOUTHWEST";
+        }else if(direction == 6){
+            directo = "WEST";
+        }else{
+            directo = "NORTHWEST";
+        }
+        
         return direction;
     }
     public int getHealth(){
@@ -127,20 +146,24 @@ public abstract class Boat
         if(x == -1){
             if( direction  == 0){
                 direction = 7;
-                an = "S1 turned left, now facing "+ getDirection();
+                getDirection();
+                an = "S1 turned left, now facing "+ directo;
             }else{
                 direction -= 1;
-                an = ("S1 turned left, now facing "+getDirection());
+                getDirection();
+                an = "S1 turned left, now facing "+ directo;
             }
         }
         else if(x == 1){
             if(direction  == 7){
                 direction = 0;
-                an = ("S1 turned right, now facing "+ getDirection());
+                getDirection();
+                an = "S1 turned right, now facing "+ directo;
             }
             else{
                 direction += 1;
-                an = ("S1 turned right, now facing "+ getDirection());
+                getDirection();
+                an = "S1 turned right, now facing "+ directo;
             }
         }
         return an;
